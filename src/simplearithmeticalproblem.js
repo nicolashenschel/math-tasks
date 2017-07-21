4f3ff6f673350e26e0a7a57bbca2077a51af7abb
Nicolas Henschel <nicolashenschel@gmail.com>
2017-07-21 21:09:15 +0200

    added level, clean up/work in progress.

--- src/simplearithmeticalproblem.js
+++ src/simplearithmeticalproblem.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import './simplearithmeticalproblem.css'
 import './w3.css'
-// import ProgressBar from './progressbar'
 
 class SimpleArithmeticalProblem extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      value1: randomNumber(),
-      value2: randomNumber(),
+      maxValue: 8,
+      value1: randomNumber(8),
+      value2: randomNumber(8),
       value: '',
       answeredQuestions: 0,
-      numberOfQuestions: 3
+      numberOfQuestions: 10
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleNewTask = this.handleNewTask.bind(this)
+    this.handleLevelChange = this.handleLevelChange.bind(this)
+  }
+
+  randomNumber () {
+    return randomNumber(this.state.maxValue)
   }
 
   handleChange (event) {
@@ -25,8 +30,12 @@ class SimpleArithmeticalProblem extends React.Component {
 
   handleNewTask (event) {
     this.setState({value: ''})
-    this.setState({value1: randomNumber()})
-    this.setState({value2: randomNumber()})
+    this.setState({value1: this.randomNumber()})
+    this.setState({value2: this.randomNumber()})
+  }
+
+  handleLevelChange (event) {
+    this.setState({maxValue: parseInt(event.target.value, 10)})
   }
 
   handleSubmit (event) {
@@ -44,12 +53,12 @@ class SimpleArithmeticalProblem extends React.Component {
   }
 
   render () {
-    let procentCompleted = Math.round(this.state.answeredQuestions / this.state.numberOfQuestions * 100) + '%'
-    let myStyle = { width: procentCompleted }
+    let procentCompleted =  this.state.numberOfQuestions + '/' + this.state.answeredQuestions
+    let myStyle = { width: Math.round(this.state.answeredQuestions / this.state.numberOfQuestions * 100) + '%' }
     return (
       <div>
+        <h1>Opgave</h1>
         <form onSubmit={this.handleSubmit}>
-        Opgave:
           <table id="rcorners2">
             <tbody>
               <tr>
@@ -74,6 +83,14 @@ class SimpleArithmeticalProblem extends React.Component {
         <div className="w3-light-grey w3-round-xlarge">
           <div className="w3-container w3-blue w3-round-xlarge" style={myStyle}>{procentCompleted}</div>
         </div>
+        <div className="w3-dropdown-click">
+          <select className="w3-button w3-bar-block w3-border w3-dropdown-hover" onChange={this.handleLevelChange}>
+            <option className="w3-dropdown-click" value="8" >Tal: 1-8</option>
+            <option className="w3-dropdown-click" value="10">Tal: 1-10</option>
+            <option className="w3-dropdown-click" value="15">Tal: 1-15</option>
+            <option className="w3-dropdown-click" value="20">Tal: 1-20</option>
+          </select>
+        </div>
       </div>
     )
   }
@@ -82,8 +99,8 @@ class SimpleArithmeticalProblem extends React.Component {
 function randomIntFromInterval (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-function randomNumber () {
-  return randomIntFromInterval(1, 10)
+function randomNumber (max) {
+  return randomIntFromInterval(1, max)
 }
 
 export default SimpleArithmeticalProblem
